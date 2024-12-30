1707e57390ec37ef3663de7b6e9568639285aa4c
hacker4world <zonegaming514@gmail.com>
2024-12-30 15:45:15 +0100

    created empty project with dependencies

---
+++ src/main/java/com/group/autoconfienceback/AutoConfienceBackApplication.java
@@ -0,0 +1,13 @@
+package com.group.autoconfienceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConfienceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfienceBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/autoconfienceback/AutoConfienceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.autoconfienceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConfienceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
